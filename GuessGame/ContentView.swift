3cb5f106ceec33d9bd7f954f21b7664c3563f7a7
Wilver Muñoz <wilver@mawiapps.com.co>
2025-06-27 09:29:34 -0500

    Creating basic buttons

--- GuessGame/ContentView.swift
+++ GuessGame/ContentView.swift
@@ -3,10 +3,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Spacer()
+                Text("Score 0")
+            }
+            Button(action:{
+                print("TEst")
+            }, label: {
+                Image("dog")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+            })
+            Button(action:{}, label: {
+                Image("cat")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+            })
+            Button(action:{}, label: {
+                Image("fox")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+            })
         }
         .padding()
     }
