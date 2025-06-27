63ddd18c3d7d095abdc5a83a15da46a0bb6cfa60
Wilver Muñoz <wilver@mawiapps.com.co>
2025-06-27 10:55:50 -0500

    Finisg guess game

--- GuessGame/ContentView.swift
+++ GuessGame/ContentView.swift
@@ -1,14 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var correctAnswer = 0
+    @State var score = 0
+    @State var titleGame = "?"
+    @State var subtitleGame = ""
+    
     var body: some View {
         VStack {
+            Text(titleGame)
+                .font(.system(size: 40, weight: .semibold))
+                .padding(.top, 100)
+            Text(subtitleGame)
+                .font(.system(size: 20, weight: .light))
+                .padding(.bottom, 100)
             HStack{
                 Spacer()
-                Text("Score 0")
+                Text("Score \(score)")
+                    .font(.system(size: 15, weight: .semibold))
             }
             Button(action:{
-                print("TEst")
+                check(answer: 0)
             }, label: {
                 Image("dog")
                     .frame(maxWidth: .infinity)
@@ -16,22 +28,54 @@ struct ContentView: View {
                     .background(Color.blue)
                     .clipShape(RoundedRectangle(cornerRadius: 6))
             })
-            Button(action:{}, label: {
+            Button(action:{
+                check(answer: 1)
+            }, label: {
                 Image("cat")
                     .frame(maxWidth: .infinity)
                     .frame(height: 50)
                     .background(Color.blue)
                     .clipShape(RoundedRectangle(cornerRadius: 6))
             })
-            Button(action:{}, label: {
+            Button(action:{
+                check(answer: 2)
+            }, label: {
                 Image("fox")
                     .frame(maxWidth: .infinity)
                     .frame(height: 50)
                     .background(Color.blue)
                     .clipShape(RoundedRectangle(cornerRadius: 6))
             })
+            Spacer()
         }
         .padding()
+    }
+    
+    func check(answer: Int){
+        if correctAnswer == answer {
+            score += 1
+            titleGame = "Success!!!"
+        } else{
+            titleGame = "Failed!!!"
+            if(score != 0){
+                score -= 1
+            }
+            
+        }
+        if (correctAnswer == 0){
+            subtitleGame = "You clicked on the dog!"
+        }
+        else if (correctAnswer == 1){
+            subtitleGame = "You clicked on the cat!"
+        }
+        else if (correctAnswer == 2){
+            subtitleGame = "You clicked on the fox!"
+        }
+        randonCorrectAnswer()
+    }
+    
+    func randonCorrectAnswer(){
+        correctAnswer = Int.random(in: 0..<3)
     }
 }
 
